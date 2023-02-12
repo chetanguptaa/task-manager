f0b6a6d62d3365dbc67b707d52c98ce8e20ad9c9
chetan gupta <chetangupta123raj@gmail.com>
2023-02-12 10:00:51 +0530

    some little changes

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express  = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+
+app.use(express.json())
+
+
+
+app.get('/hello', () => {
+    res.end("hello world");
+})
+
+app.use('/api/v1/tasks', tasks)
+
+app.listen(3000, console.log("server is listening on port 3000..."))
