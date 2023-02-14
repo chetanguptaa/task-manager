e5b34058332b830035742cfb1fc81e2b5aa56594
chetan gupta <chetangupta123raj@gmail.com>
2023-02-14 17:42:26 +0530

    updated app.js

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,28 +1,59 @@
-const express  = require('express');
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
-require('dotenv').config();
+// const express  = require('express');
+// const tasks = require('./routes/tasks');
+// const connectDB = require('./db/connect');
+// require('dotenv').config();
+// const app = express();
+
+// app.use(express.json())
+
+
+
+// app.get('/', () => {
+//         res.send('Task Manager App')
+// })
+
+// app.use('/api/v1/tasks', tasks)
+
+// const port = 3000;
+
+// const start =  async () => {
+//         try {
+//                 await connectDB(process.env.MONGO_URI)
+//                 app.listen(port, console.log(`server is listening on port ${port}...`))
+//         } catch (error) {
+//                 console.log(error)
+//         }
+// }
+
+// start();
+const express = require('express');
+const { connectToDb, getDb } = require('./db/connect');
+
 const app = express();
 
-app.use(express.json())
+//db connection
+let db;
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log('app is listening on port 3000...');
+    });
+    db = getDb();
+  } else {
+    console.log(err);
+  }
+});
 
-
-
-app.get('/', () => {
-        res.send('Task Manager App')
-})
-
-app.use('/api/v1/tasks', tasks)
-
-const port = 3000;
-
-const start =  async () => {
-        try {
-                await connectDB(process.env.MONGO_URI)
-                app.listen(port, console.log(`server is listening on port ${port}...`))
-        } catch (error) {
-                console.log(error)
-        }
-}
-
-start();
+app.get('/books', (req, res) => {
+  db.collection('books')
+    .find()
+    .sort({ author: 1 })
+    .toArray()
+    .then((books) => {
+      res.status(200).json(books);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: 'could not fetch' });
+    });
+});
--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,15 +1,32 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
 
-const connectDB = (url) => {
-    return mongoose.connect(url, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
+// const connectDB = (url) => {
+//     return mongoose.connect(url, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//         useUnifiedTopology: true,
+//     })
+// }
+
+// module.exports = connectDB
+let dbConnection;    
+
+const { MongoClient } = require('mongodb');
+
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient.connect('mongodb://admin:password@localhost:27017/bookstore',{useNewUrlParser:true,useUnifiedTopology:true})
+            .then((client) => {
+                dbConnection = client.db()
+                return cb()
+            })
+            .catch(err => {
+                console.log(err)
+                return cb(err)
+            })
+    },
+    getDb: () => dbConnection
 }
-
-module.exports = connectDB
-    
