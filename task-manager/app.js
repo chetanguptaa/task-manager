61c590286b1e9d68f406b36d6b0dc10e0bdbb47b
chetan gupta <chetangupta123raj@gmail.com>
2023-02-18 18:08:31 +0530

    added and updated controller, middleware, routes, and app.js

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,66 +1,22 @@
 const express = require('express');
 const app =  express();
-const Task = require('./models/Task');
-const mongoose = require('mongoose')
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+require('dotenv').config();
+const tasks = require('./routes/tasks');
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+
 
 app.use(express.static('./public'));
 app.use(express.json());
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-
-app.get('/api/v1/tasks', (req, res) => {
-        const tasks = Task.find({});
-        res.status(200).json({ tasks })
-})
-
-app.post('/api/v1/tasks', (req, res)=> {
-        const task = Task.create(req.body);
-        res.status(201).json({task})
-})
-
-app.get('/api/v1/tasks/:id', (req, res, next) => {
-        const { id: taskID } = req.params;
-        const task = Task.findOne({_id: taskID });
-        if ( !task) {
-                return next(`no task with id ${taskID}`, 404);
-        } 
-        res.status(200).json({task});
-});
-
-app.delete('/api/v1/tasks/:id', (req, res, next) => {
-        const { id: taskID} = req.params;
-        const task = Task.findOneAndDelete({_id: taskID});
-        if ( !task) {
-                return next(`no task with id ${taskID}`, 404);
-        }
-        res.status(200).json({task})
-});
-
-app.put('/api/v1/tasks/:id', (req, res , next) => {
-        const { id : taskID} = req.params;
-        const task = Task.findOneAndUpdate({_id: taskID}, req.body, {
-                new: true,
-                runValidators: true,
-        })
-        if ( !task ) {
-                return next(`no task with id ${taskID}`, 404);
-        }
-        res.status(200).json({ task })
-})
 
 
 const port = 3000;
-// app.listen(port,() => {
-//         console.log(`app is listening on port ${port}`)
-// })
 
 const start = async () => {
         try {
