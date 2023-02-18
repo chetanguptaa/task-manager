5b229dd1d214ae5e336d7fde08f123f8bc3d36f5
chetan gupta <chetangupta123raj@gmail.com>
2023-02-18 17:32:41 +0530

    updated app.js

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,35 +1,75 @@
 const express = require('express');
 const app =  express();
+const Task = require('./models/Task');
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+}
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+
+app.get('/api/v1/tasks', (req, res) => {
+        const tasks = Task.find({});
+        res.status(200).json({ tasks })
+})
+
+app.post('/api/v1/tasks', (req, res)=> {
+        const task = Task.create(req.body);
+        res.status(201).json({task})
+})
+
+app.get('/api/v1/tasks/:id', (req, res, next) => {
+        const { id: taskID } = req.params;
+        const task = Task.findOne({_id: taskID });
+        if ( !task) {
+                return next(`no task with id ${taskID}`, 404);
+        } 
+        res.status(200).json({task});
+});
+
+app.delete('/api/v1/tasks/:id', (req, res, next) => {
+        const { id: taskID} = req.params;
+        const task = Task.findOneAndDelete({_id: taskID});
+        if ( !task) {
+                return next(`no task with id ${taskID}`, 404);
+        }
+        res.status(200).json({task})
+});
+
+app.put('/api/v1/tasks/:id', (req, res , next) => {
+        const { id : taskID} = req.params;
+        const task = Task.findOneAndUpdate({_id: taskID}, req.body, {
+                new: true,
+                runValidators: true,
+        })
+        if ( !task ) {
+                return next(`no task with id ${taskID}`, 404);
+        }
+        res.status(200).json({ task })
+})
+
 
 const port = 3000;
+// app.listen(port,() => {
+//         console.log(`app is listening on port ${port}`)
+// })
 
-const myLogger = (req, res,next) => {
-        console.log("LOGGED");
-        next();
-}
-const requestTime = (req, res,next) => {
-        req.requestTime = Date.now();
-        res.end();
-}
-
-
-app.use(express.static('public'))
-app.use(myLogger);
-app.use(requestTime);
-
-
-app.get('/', (req, res) => {
-        let responseText = 'Hello World!<br>';
-        responseText = `<small>Requested at: ${req.requestTime}</small>`;
-        res.send(responseText)
-})
-app.post('/',(req, res) => {
-        res.send('Got a POST request')
-})
-app.put('/user', (req, res) => {
-        res.send('Got a put request at /user')
-})
-
-app.listen(port,() => {
-        console.log(`app is listening on port ${port}`)
-})
+const start = async () => {
+        try {
+                await connectDB('mongodb://admin:password@localhost:27017');
+                app.listen(port, () => {
+                        console.log(`server is listening on port ${port}...`)
+                });
+        } catch (error) {
+                console.log(error);
+        }
+};
+start();
