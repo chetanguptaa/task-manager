e86065e59610518a189323f28bc873bfb2a74c8d
chetan gupta <chetangupta123raj@gmail.com>
2023-03-15 18:51:25 +0530

    updated main.tf and providers.tf

--- task-manager/terraform/main.tf
+++ task-manager/terraform/main.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "tm_vpc" {
-  cidr_block = "10.169.0.0/24"
+  cidr_block           = "10.169.0.0/24"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     Name = "task-manager"
   }
 }
 resource "aws_subnet" "tm-public_subnet" {
-  vpc_id = aws_vpc.tm_vpc.id
-  cidr_block = "10.169.3.0/24"
+  vpc_id                  = aws_vpc.tm_vpc.id
+  cidr_block              = "10.169.3.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1"
+  availability_zone       = "us-east-1"
   tags = {
     Name = "tm-public"
   }
@@ -31,13 +31,13 @@ resource "aws_route_table" "tm-rt" {
 }
 
 resource "aws_route" "default-route" {
-  route_table_id = aws_route_table.tm-rt.id
+  route_table_id         = aws_route_table.tm-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.tm-itw.id
+  gateway_id             = aws_internet_gateway.tm-itw.id
 }
 
 resource "aws_route_table_association" "tm-rta" {
-  subnet_id = aws_subnet.tm_public_subnet.id
+  subnet_id      = aws_subnet.tm_public_subnet.id
   route_table_id = aws_route_table.tm-rt.id
 }
 
@@ -65,16 +65,16 @@ resource "aws_key_pair" "my_auth" {
 }
 
 resource "aws_instance" "node" {
-  instance_type = "t2.micro"
-  ami = data.aws_ami.server_ami.id
-  key_name = aws_key_pair.my_auth.id
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.my_auth.id
   vpc_security_group_ids = [aws_security_group.tm-sg.id]
-  subnet_id = aws_subnet.tm-public_subnet.id
-  user_data = file("userdate.tpl")
+  subnet_id              = aws_subnet.tm-public_subnet.id
+  user_data              = file("userdate.tpl")
   root_block_device {
     volume_size = 10
   }
-  tags =  {
+  tags = {
     Name = "node"
   }
 }
